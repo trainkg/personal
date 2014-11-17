fd61c61ff0a60219e2139715a3d22e67eefedaff
朱元宇 <peculiar.1@163.com>
2014-11-17 21:42:04 +0800

    第一版本

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 personal
 ========
 
-个人日常总结/积累
-
-
-THIS IS TEST INFO
+personal workspace , wicket use case etc
---
+++ wicket/zsq.wicket/chapter-1/src/main/java/com/wiket/controller/CApplication.java
@@ -0,0 +1,37 @@
+package com.wiket.controller;
+
+import org.apache.wicket.Application;
+import org.apache.wicket.Page;
+import org.apache.wicket.RuntimeConfigurationType;
+import org.apache.wicket.Session;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.wiket.web.CHomePage;
+
+/**
+ * 
+ * @author Administrator
+ */
+public class CApplication extends WebApplication {
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
+	@Override
+	protected void init() {
+		super.init();
+	}
+
+	@Override
+	public Class<? extends Page> getHomePage() {
+		return CHomePage.class;
+	}	
+
+	@Override
+	public Session newSession(Request paramRequest, Response paramResponse) {
+		return null;
+	}
+
+}
